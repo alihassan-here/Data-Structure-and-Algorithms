4b42343695d7c0eff996271e474a1c0e2f480bbf
Ali Hassan <82035974+alihassan-here@users.noreply.github.com>
2026-04-24 07:17:37 +0000

    added more problems

--- Problem Solving/CharCount.js
+++ Problem Solving/CharCount.js
@@ -13,14 +13,14 @@
 //     return obj;
 // }
 
-// function charCount(str) {
-//     var obj = {};
-//     for (var char of str){
-//         char=char.toLowerCase();
-//         if(/[a-z0-9]/.test(char)){
-//             obj[char]=++obj[char]||1;
-//         }
-//     }
-//     return obj;
-// }
+function charCount(str) {
+    var obj = {};
+    for (var char of str){
+        char=char.toLowerCase();
+        if(/[a-z0-9]/.test(char)){
+            obj[char]=++obj[char]||1;
+        }
+    }
+    return obj;
+}
 console.log(charCount("hellow world"));
